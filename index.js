801790cc5fdce82b39060dd8c5853d3181d8585d
gopal172002 <gopalkalsiya1h@gmail.com>
2023-09-14 19:40:47 +0530

    my changes

--- index.js
+++ index.js
@@ -1,40 +1,39 @@
 const mongoose = require("mongoose");
-const msgSchema = new mongoose.Schema({
-  message: {
-    type: String,
-    required: true,
-  },
-  name: {
-    type: String,
-    required: true,
-  },
-  room: {
-    type: String,
-    required: true,
-  },
-  lastUpdatedAt: {
-    type: Date,
-    default: Date.now,
-  },
-  select: {
-    type: String,
-    required: true,
-  },
-});
-const { MongoClient } = require('mongodb');
-const Msg = require("../models/chatModel");
+const { Msg } = require("./models/chatModel");
+const cron = require("node-cron");
+const CryptoJS = require("crypto-js");
+const bcrypt = require("bcryptjs");
 
-// Node Server which will handle socket connections.
+mongoose.connect("mongodb+srv://gopalkalsiya1h:Gopal172002@cluster0.ggzm5fi.mongodb.net/chatapp", {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+});
+
+const db = mongoose.connection;
+db.on("error", console.error.bind(console, "MongoDB connection error:"));
+db.once("open", () => {
+  console.log("Connected to MongoDB");
+});
+
 const io = require("socket.io")(8000, {
     cors: {
         origin: "*",
     },
 });
-// const MONGO_URI = process.env.URI;
-// const { name } = require('ejs');
 
 const users = {};
-// const client = new MongoClient(uri);
+
+//cronjob scheduling...
+cron.schedule("0 0 * * *", async () => {
+  try {
+    // Delete all chats by removing all documents from the Msg collection
+    await Msg.deleteMany({});
+    console.log("All chats deleted at midnight.");
+  } catch (error) {
+    console.error("Error deleting chats:", error);
+  }
+});
+
 
 io.on("connection", (socket) => {
     socket.on("new-user-joined", async(name) => {
@@ -43,15 +42,36 @@ io.on("connection", (socket) => {
         socket.broadcast.emit("user-joined", name);
     });
 
-    socket.on("send", (message) => {
-
+    socket.on("send", async (message) => {
+      try {
+        
+        const secretKey = "your-secret-key"; 
+        const encryptedMessage = CryptoJS.AES.encrypt(message, secretKey).toString();
+    
+        // Hash the encrypted message before saving it
+        const hashedMessage = await bcrypt.hash(encryptedMessage, 10);
+    
+        
+        const newMessage = new Msg({
+          message: hashedMessage, // hashed message
+          name: users[socket.id], 
+          room: "2", 
+          select: "2", // selct
+        });
+    
+        // Save the message to MongoDB
+        await newMessage.save();
+    
         
         socket.broadcast.emit("receive", {
-            message: message,
-            name: users[socket.id],
+          message: message, 
+          name: users[socket.id],
         });
+      } catch (error) {
+        console.error("Error saving message:", error);
+      }
     });
-
+    
     socket.on("disconnect", (message) => {
         socket.broadcast.emit("left", users[socket.id]);
         delete users[socket.id];
